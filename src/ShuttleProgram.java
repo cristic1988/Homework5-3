4505a4be33d43bcf259f11aa4a5f3929f0e96024
cristic1988 <cristic8@gmail.com>
2024-03-11 22:50:07 +0200

    second commit

--- src/ShuttleProgram.java
+++ src/ShuttleProgram.java
@@ -2,20 +2,21 @@ public class ShuttleProgram {
 
     public static void main(String[] args) {
 
-        for (int i = 0; i <= 100; i++) {
-            if (i % 4 == 0) {
-                continue;
-            }
-            if (i % 9 == 0){
-                continue;
-            }
+       for ( int i = 0; i <= 100; i++){
+            if (i % 4 / 10 == 0 || i % 9 / 10 == 0) {
+                i = 0;
+                if (i <= 100)
+                    System.out.println("shuttle № " + "  " + i);
+                else
+                    System.out.println("shuttle № " + " 0 " + i);
 
-
-            System.out.println("shuttle № " + i);
             }
+            }
+        }
+
 
         }
-    }
+
 
 
 
